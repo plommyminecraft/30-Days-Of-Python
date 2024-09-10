124b908618c066a6210be3494cb314c2ea568b04
plommyminecraft <plommyminecraft@gmail.com>
2024-09-10 13:26:23 +0000

    stuff

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,7 +1,7 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge!
 
-print(2 + 3)   # addition(+)
+print(10 + 3)   # addition(+)
 print(3 - 1)   # subtraction(-)
 print(2 * 3)   # multiplication(*)
 print(3 / 2)   # division(/)
